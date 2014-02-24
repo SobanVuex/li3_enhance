df192fc90015a4ad1fcefa14a63d6219136aa0b4
SobanVuex <soban.vue@gmail.com>
2014-02-24 15:17:42 -0500

    Add log() to Model. Fix Model inheritence

--- models/Model.php
+++ models/Model.php
@@ -10,12 +10,49 @@
 
 namespace li3_enhance\models;
 
+use lithium\analysis\Logger;
+
 /**
  * Base application model
  *
  * @package li3_enhance
  */
-class Model
+class Model extends \lithium\data\Model
 {
-    
+
+    /**
+     * @var array
+     */
+    protected $loggerPriorities = array(
+        LOG_EMERG => 'emergency',
+        LOG_ALERT => 'alert',
+        LOG_CRIT => 'critical',
+        LOG_ERR => 'error',
+        LOG_WARNING => 'warning',
+        LOG_NOTICE => 'notice',
+        LOG_INFO => 'info',
+        LOG_DEBUG => 'debug',
+    );
+
+    /**
+     * @param  array|string $message
+     * @param  integer      $flag http://php.net/manual/en/function.syslog.php
+     * @return boolean
+     */
+    protected function log($message, $flag = LOG_WARNING)
+    {
+        if (isset($this->loggerPriorities[$flag])) {
+            $flag = LOG_WARNING;
+        }
+
+        if (!is_array($message)) {
+            $message = array('message' => $message);
+        }
+
+        $message += array('model' => get_called_class());
+
+        var_dump($message);
+        return (boolean) Logger::write($this->loggerPriorities[$flag], json_encode($message));
+    }
+
 }
