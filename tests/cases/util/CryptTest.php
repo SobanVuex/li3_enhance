04e5052780ac2f98142b17cc29b1ae36e57cd5b2
SobanVuex <soban.vue@gmail.com>
2014-02-27 16:34:49 -0500

    Add crypting utility tests

---
+++ tests/cases/util/CryptTest.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * Li3 Enhance - Collection of extensions and utilities for li3
+ * 
+ * @copyright 2014 Alex Soban. See LICENSE for information.
+ * @license   http://opensource.org/licenses/MIT
+ * @author    Alex Soban <me@soban.co>
+ */
+
+namespace li3_enhance\tests\cases\util;
+
+use li3_enhance\util\Crypt;
+
+/**
+ * @package li3_enhance
+ */
+class CryptTest extends \lithium\test\Unit
+{
+
+    public function setUp()
+    {
+        $this->password = 'ps010ITXWGRfA';
+    }
+
+    public function testEncryptBoolean()
+    {
+        $this->assertNotEmpty(Crypt::encrypt(true, $this->password));
+        $this->assertNotEmpty(Crypt::encrypt(false, $this->password));
+    }
+
+    public function testEncryptNumeric()
+    {
+        $this->assertNotEmpty(Crypt::encrypt(123, $this->password));
+        $this->assertNotEmpty(Crypt::encrypt(123.4, $this->password));
+    }
+
+    public function testEncryptString()
+    {
+        $this->assertNotEmpty(Crypt::encrypt('abc', $this->password));
+    }
+
+    public function testEncryptArray()
+    {
+        $this->assertNotEmpty(Crypt::encrypt(array(1, 2, 3), $this->password));
+        $this->assertNotEmpty(Crypt::encrypt(array(), $this->password));
+    }
+
+    public function testEncryptObject()
+    {
+        $this->assertNotEmpty(Crypt::encrypt(new \stdClass(), $this->password));
+    }
+
+    public function testEncryptResource()
+    {
+        $this->expectException('Type `resource` can not be encrypted.');
+        Crypt::encrypt(fopen(__FILE__, 'r'), $this->password);
+    }
+
+    public function testDecryptBoolean()
+    {
+        $encrypted = Crypt::encrypt(true, $this->password);
+        $this->assertIdentical(true, Crypt::decrypt($encrypted, $this->password));
+
+        $encrypted = Crypt::encrypt(false, $this->password);
+        $this->assertIdentical(false, Crypt::decrypt($encrypted, $this->password));
+    }
+
+    public function testDecryptNumeric()
+    {
+        $encrypted = Crypt::encrypt(123, $this->password);
+        $this->assertIdentical(123, Crypt::decrypt($encrypted, $this->password));
+//
+        $encrypted = Crypt::encrypt(123.4, $this->password);
+        $this->assertIdentical(123.4, Crypt::decrypt($encrypted, $this->password));
+    }
+
+    public function testDecryptString()
+    {
+        $encrypted = Crypt::encrypt('abc', $this->password);
+        $this->assertIdentical('abc', Crypt::decrypt($encrypted, $this->password));
+    }
+
+    public function testDecryptArray()
+    {
+
+        $value = array(1, 2, 3);
+        $encrypted = Crypt::encrypt($value, $this->password);
+        $this->assertIdentical($value, Crypt::decrypt($encrypted, $this->password));
+
+        $value = array();
+        $encrypted = Crypt::encrypt($value, $this->password);
+        $this->assertIdentical($value, Crypt::decrypt($encrypted, $this->password));
+    }
+
+    public function testDecryptObject()
+    {
+        $encrypted = Crypt::encrypt(new \stdClass(), $this->password);
+        $this->assertInstanceOf('stdClass', Crypt::decrypt($encrypted, $this->password));
+    }
+
+}
